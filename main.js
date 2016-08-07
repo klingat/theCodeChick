b0f8cdcfceef98d36dcbbc5f861e26fe9949fd84
klingat <klingat@me.com>
2016-08-07 19:20:52 -0400

    new site

---
+++ main.js
@@ -0,0 +1,17 @@
+//
+//  $(window).load(function() {
+//			// Animate loader off screen
+//			$("#loader").animate({
+//				top: -200
+//			}, 1500);
+//		});
+//$(document).ready(function() {
+//  $(window).load(function(){
+//     $('#loader').fadeOut();
+//});
+//})  
+//
+
+$(window).load(function() {
+	$(".loader").fadeOut("slow");
+})
